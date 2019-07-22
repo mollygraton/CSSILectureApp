b899c20b5480d0b51c6802aceb43ec60b25816f7
Chris Cossyleon <chriscossyleon@gmail.com>
2019-07-22 10:56:35 -0700

    hi

---
+++ main.py
@@ -0,0 +1,28 @@
+import webapp2
+import jinja2
+import os
+from google.appengine.api import users
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+
+ ############################################################################
+
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        user = users.get_current_user()
+        template = JINJA_ENVIRONMENT.get_template('templates/main.html')
+        data = {
+          'user': user,
+          'login_url': users.create_login_url(self.request.uri),
+          'logout_url': users.create_logout_url(self.request.uri),
+        }
+        self.response.headers['Content-Type'] = 'text/html'
+        self.response.write(template.render(data))
+
+# The app config
+app = webapp2.WSGIApplication([
+    ('/', MainPage),
+], debug=True)
