7ba43cadbcf26f63db46c87df4b40196bf36d06b
sgire421 <sophie.g.gire@gmail.com>
2019-07-22 13:11:38 -0700

    mainpage goes to index.html

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ JINJA_ENVIRONMENT = jinja2.Environment(
 class MainPage(webapp2.RequestHandler):
     def get(self):
         user = users.get_current_user()
-        template = JINJA_ENVIRONMENT.get_template('templates/main.html')
+        template = JINJA_ENVIRONMENT.get_template('templates/index.html')
         data = {
           'user': user,
           'login_url': users.create_login_url(self.request.uri),
