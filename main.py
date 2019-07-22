2bef110df64725c5322b562af463157b0c5834e7
Santiago V <svalenciasan@gmail.com>
2019-07-22 15:53:13 -0500

    Fist of 5 added to studentSession

--- main.py
+++ main.py
@@ -40,13 +40,8 @@ class StudentSessionPage(webapp2.RequestHandler):
     def get(self):
         user = users.get_current_user()
         template = JINJA_ENVIRONMENT.get_template('templates/StudentSession.html')
-        data = {
-          'user': user,
-          'login_url': users.create_login_url(self.request.uri),
-          'logout_url': users.create_logout_url(self.request.uri),
-        }
         self.response.headers['Content-Type'] = 'text/html'
-        self.response.write(template.render(data))
+        self.response.write(template.render())
 
 class TeacherDashboardPage(webapp2.RequestHandler):
     def get(self):
