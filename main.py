fbcb91be30a67ae2707cd718661f458133b94f5e
Molly G <molly.graton@gmail.com>
2019-07-22 16:37:16 -0500

    Update main.py

--- main.py
+++ main.py
@@ -34,12 +34,13 @@ class StudentDashboardPage(webapp2.RequestHandler):
 class StudentSessionPage(webapp2.RequestHandler):
     def get(self):
         user = users.get_current_user()
-        sCode = self.request.get('code')
+        sCode = int(self.request.get('code'))
         tCode = 891
         if (sCode==tCode):
             template = JINJA_ENVIRONMENT.get_template('templates/StudentSession.html')
             self.response.headers['Content-Type'] = 'text/html'
             self.response.write(template.render())
+
 
 class TeacherDashboardPage(webapp2.RequestHandler):
     def get(self):
