41e77e26bfce8be2fb34820b4f8170cf5aaa2764
Molly G <molly.graton@gmail.com>
2019-07-24 15:38:57 -0500

    Chart Live

--- js/main.js
+++ js/main.js
@@ -19,6 +19,9 @@ function fetchCurrentNote() {
       }
       stringFun += `<input type="submit" name="" value="Delete Selected"></input>`
       note_div.innerHTML = stringFun
+
+      //update the chart
+
       // Start the timer again for the next request.
       startTimer(10000)
     })
@@ -37,3 +40,48 @@ if (note_div != null) {
   // Start by fetching the current note without any delay.
   fetchCurrentNote()
 }
+
+function fetchCurrentChart() {
+  fetch('/ajax/get_current_chat')
+    .then(function(response) {
+      return response.json()
+    })
+    .then(function (myJson) {
+      // Update the div.
+      var stringFun = ""
+      note_div.innerHTML = ""
+      for(x=0;x<myJson.question.length;x++){
+        stringFun += `<input type="checkbox" name="to_delete" value="${myJson.question[x].key}">${myJson.question[x].question_text}</br>`
+      }
+      stringFun += `<input type="submit" name="" value="Delete Selected"></input>`
+      note_div.innerHTML = stringFun
+
+      //update the chart
+
+      // Start the timer again for the next request.
+      startTimer(10000)
+    })
+}
+function drawChart() {
+
+    // Create the data table.
+    var data = new google.visualization.DataTable();
+    data.addColumn('string', 'Understanding');
+    data.addColumn('number', 'Students');
+    total1 = {{numOf1}}
+    total2 = {{numOf2}}
+    total3 = {{numOf3}}
+    total4 = {{numOf4}}
+    total5 = {{numOf5}}
+    data.addRows([
+      ['1', total1],
+      ['2', total2],
+      ['3', total3],
+      ['4', total4],
+      ['5', total5]
+    ]);
+
+    var options = {'title':'General Class Comprehension'};
+    var chart = new google.visualization.BarChart(document.getElementById('chart_div'));
+    chart.draw(data, options);
+  }
